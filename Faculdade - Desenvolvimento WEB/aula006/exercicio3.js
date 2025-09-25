c32031ee80f0db70340952ad0172a906d62c2f04
Aluno <aluno@alunos.ce.senac.br>
2025-09-25 10:43:03 -0300

    Comitando ultimas anotações da aula

---
+++ Faculdade - Desenvolvimento WEB/aula006/exercicio3.js
@@ -0,0 +1,19 @@
+var idade
+var idade_selecionada = 23
+
+if(idade < 12){
+    var idade_selecionada = "crianca"
+}
+
+else if(idade >= 12 && idade <= 17){
+    var idade_selecionada = "adolescente"
+}
+
+else if(idade >= 18 && idade <= 59){
+    var idade_selecionada = "Adulto"
+}
+
+else if(idade > 60){
+    var idade_selecionada = "idoso"
+}
+
---
+++ Faculdade - Desenvolvimento WEB/aula006/exercicio4.js
@@ -0,0 +1,16 @@
+var num1
+var num2
+var operacao 
+
+if(operacao == "+"){
+    console.log(num1 + num2)
+}
+else if(operacao == "-"){
+    console.log(num1 - num2)
+}
+else if(operacao == "*"){
+    console.log(num1 * num2)
+}
+else if(operacao == "/"){
+    console.log(num1 / num2)
+}
